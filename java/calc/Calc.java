d4a118bae955c85e6a9e04994db1fc237189c671
ElectroForez <vlad.tancev@gmail.com>
2022-04-04 08:33:14 +0300

    Добавлена спецификация на Calc

--- java/calc/Calc.java
+++ java/calc/Calc.java
@@ -9,15 +9,15 @@ public class Calc {
     final static char BRACE_C = ')';
     final static char BRACE_O = '(';
     final static char UNDEFINED = 'U';
-    static HashMap<String, Integer> priority= getPriority();
+    final static HashMap<String, Integer> PRIORITY= getPriority();
     
     public static void main(String args[]) {
 	String expr = "1 + 2 * 7 + 4 / 2 - 10"; // 7
 	System.out.println(expr);
-	System.out.println(calc(expr));
+	System.out.println(exprCalc(expr));
     }
 
-    public static int calc(String expr) {
+    public static int exprCalc(String expr) {
 	String tokens[] = expr.split(" ");
 	Stack<Integer> stackNum = new Stack<Integer>();
 	Stack<String> stackOp = new Stack<String>();
@@ -33,21 +33,21 @@ public class Calc {
 		stackOp.push(token);
 	    } else if (token.equals(")")){
 		while (!stackOp.peek().equals("(")) {
-		    calc_upper(stackNum, stackOp);
+		    calcUpper(stackNum, stackOp);
 		}
 		stackOp.pop();
 	    } else {
-		int cur_prior = priority.get(token);
-		if (stackOp.empty() || stackOp.peek().equals("(")) {
+    		if (stackOp.empty() || stackOp.peek().equals("(")) {
 		    stackOp.push(token);
 		    continue;
 		}
 		
-		int last_prior = priority.get(stackOp.peek());
-		if (last_prior < cur_prior)
+		int curPrior = PRIORITY.get(token);
+		int lastPrior = PRIORITY.get(stackOp.peek());
+		if (lastPrior < curPrior)
 		    stackOp.push(token);
 		else {
-		    calc_upper(stackNum, stackOp);
+		    calcUpper(stackNum, stackOp);
 		    stackOp.push(token);
 		}
 	    }
@@ -55,15 +55,15 @@ public class Calc {
 	}
 	
 	while (!stackOp.empty()) {
-	    calc_upper(stackNum, stackOp);
+	    calcUpper(stackNum, stackOp);
 	}
 	return stackNum.pop();
     }
 
     public static boolean isCorrect(String tokens[]) {
 	char lastType = UNDEFINED;
-	Stack<String> all_token = new Stack<String>();
-	all_token.push("");
+	Stack<String> stackToken = new Stack<String>();
+	stackToken.push("");
 	Stack<String> braces = new Stack<String>();
 	for (String token: tokens) {
 	    char tokenType = getTokenType(token);
@@ -73,18 +73,18 @@ public class Calc {
 		return false;
 	    } else if (tokenType == NUMBER) {
 		if (lastType == NUMBER || lastType == BRACE_C) {
-		    System.out.println("Неправильный порядок: " + all_token.pop() + " " + token);
+		    System.out.println("Неправильный порядок: " + stackToken.pop() + " " + token);
 		    return false;
 		}
 	    }  else if (tokenType == BRACE_O) {
 		if (lastType == BRACE_C || lastType == NUMBER) {
-		    System.out.println("Неправильный порядок: " + all_token.pop() + " " + token);
+		    System.out.println("Неправильный порядок: " + stackToken.pop() + " " + token);
 		    return false;
 		}
 		braces.push(token);
 	    } else if (tokenType == BRACE_C) {
 		if (lastType == OPERATOR || lastType == UNDEFINED || lastType == BRACE_O) {
-		    System.out.println("Неправильный порядок: " + all_token.pop() + " " + token);
+		    System.out.println("Неправильный порядок: " + stackToken.pop() + " " + token);
 		    return false;
 		}
 
@@ -95,12 +95,12 @@ public class Calc {
 		braces.pop();
 	    } else if (tokenType == OPERATOR) {
 		if (lastType != NUMBER && lastType != BRACE_C) {
-		    System.out.println("Неправильный порядок: " + all_token.pop() + " " + token);
+		    System.out.println("Неправильный порядок: " + stackToken.pop() + " " + token);
 		    return false;
 		}
 	    }
 	    lastType = tokenType;
-	    all_token.push(token);
+	    stackToken.push(token);
 	}
 	if (!braces.empty()) {
 	    System.out.println("Не хватает скобочки )");
@@ -117,23 +117,23 @@ public class Calc {
 	    return BRACE_C;
 	if (token.equals("("))
 	    return BRACE_O;
-	if (priority.containsKey(token))
+	if (PRIORITY.containsKey(token))
 	    return OPERATOR;
 	return UNDEFINED;
     }
     public static HashMap<String, Integer> getPriority() {
-	HashMap<String, Integer> priority= new HashMap<String, Integer>();
-	priority.put("+", 1);
-	priority.put("-", 1);
-	priority.put("*", 2);
-	priority.put("/", 2);
-	return priority;
+	HashMap<String, Integer> PRIORITY= new HashMap<String, Integer>();
+	PRIORITY.put("+", 1);
+	PRIORITY.put("-", 1);
+	PRIORITY.put("*", 2);
+	PRIORITY.put("/", 2);
+	return PRIORITY;
     }
 
-    public static void calc_upper(Stack<Integer> stackNum, Stack<String> stackOp) {
+    public static void calcUpper(Stack<Integer> stackNum, Stack<String> stackOp) {
 	int r_op = stackNum.pop();
 	int l_op = stackNum.pop();
-	int result = calculate_2(l_op, r_op, stackOp.pop());
+	int result = binCalc(l_op, r_op, stackOp.pop());
 	stackNum.push(result);
     }
     
@@ -141,7 +141,7 @@ public class Calc {
 	return str.matches("[-+]?\\d+");
     }
     
-    public static int calculate_2(int a, int b, String op) {
+    public static int binCalc(int a, int b, String op) {
 	int result;
 	switch (op) {
 	case "+":
