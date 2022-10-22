2aadd04ef6544b57b961f95f1c5b12fa8dc0cb93
Vlad Tancev <vlad.tancev@gmail.com>
2022-10-22 10:32:48 +0300

    Add user icon

--- mdk_02.01/authorize/main.py
+++ mdk_02.01/authorize/main.py
@@ -1,5 +1,8 @@
 import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
+from PyQt5.QtGui import QPainter, QPixmap
+from PyQt5.QtWidgets import QLineEdit
+
 import design  # Это наш конвертированный файл дизайна
 from DbHandler import DbHandler
 
@@ -20,9 +23,12 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
             user = self.db_handler.get_user(login, password)
             if user:
                 msg_box = QtWidgets.QMessageBox()
+                painter = QPainter(msg_box)
+                pixmap = QPixmap(f"img/{user[2].split()[0]}.jpeg")
                 msg_box.setText('Успешная авторизация:\n'
                                 f'ФИО: {user[2]}\n'
                                 f'Должность: {user[1]}\n')
+                msg_box.setIconPixmap(pixmap)
                 msg_box.exec()
             else:
                 msg_box = QtWidgets.QMessageBox()
@@ -31,14 +37,25 @@ class ExampleApp(QtWidgets.QMainWindow, design.Ui_MainWindow):
         else:
             print('Поля пусты')
 
+    def click_show_pass(self):
+        if self.check_show_pass.isChecked():
+            self.pass_edit.setEchoMode(QLineEdit.Normal)
+        else:
+            self.pass_edit.setEchoMode(QLineEdit.Password)
+
     def init_logic(self):
         self.auth_btn.clicked.connect(self.click_button)
+        self.login_edit.setText("Ivanov@namecomp.ru")
+        self.pass_edit.setText("2L6KZG")
+        self.check_show_pass.stateChanged.connect(self.click_show_pass)
+
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
     window = ExampleApp()  # Создаём объект класса ExampleApp
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
+
 
 if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
     main()  # то запускаем функцию main()
